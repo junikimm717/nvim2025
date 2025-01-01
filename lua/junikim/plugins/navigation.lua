a41ddff235e3dc16ba67a9c841bad044d13128f4
Juni Kim <junikimm717@gmail.com>
2025-01-01 12:32:56 -0500

    oil.nvim added instead of filetree

--- lua/junikim/plugins/navigation.lua
+++ lua/junikim/plugins/navigation.lua
@@ -119,16 +119,14 @@ return {
     end,
   },
   {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      local api = require("nvim-tree.api")
-      vim.keymap.set("n", "<C-n>", api.tree.toggle)
-      require("nvim-tree").setup {}
-    end,
-  },
+      require("oil").setup()
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    end
+  }
 }
