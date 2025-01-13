417c0265ac0027d9b4d5ad7ef706839484f18028
Juni Kim <junikimm717@gmail.com>
2025-01-13 13:46:12 -0500

    dockerignore update

--- lua/junikim/utils.lua
+++ lua/junikim/utils.lua
@@ -8,4 +8,22 @@ M.getWords = function()
   return tostring(wordcount.words) .. " W"
 end
 
+M.read_json = function(filename)
+  filename = vim.fs.normalize(filename)
+  if not vim.fn.filereadable(filename) then
+    return nil
+  end
+  local content = vim.fn.readfile(filename)
+  if not content or #content == 0 then
+    return nil
+  end
+  content = table.concat(content, "\n")
+  local success, res = pcall(vim.json.decode, content)
+  if not success then
+    return nil
+  else
+    return res
+  end
+end
+
 return M
