f5727691806d3105e4793533eeb99ba90971c408
Juni Kim <junikimm717@gmail.com>
2025-02-20 11:30:08 -0500

    custom snippets first

--- lua/junikim/plugins/lsp.lua
+++ lua/junikim/plugins/lsp.lua
@@ -288,10 +288,10 @@ return {
       "rafamadriz/friendly-snippets",
     },
     config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
       require("luasnip.loaders.from_snipmate").lazy_load({
         paths = { "./lua/junikim/snippets" },
       })
-      require("luasnip.loaders.from_vscode").lazy_load()
     end,
   },
 }
